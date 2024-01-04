af1bcc00cbd16777ebcc93f28a1d1ba03666cefc
nestaraus <nestaraus@mail.pu>
2024-01-04 17:26:51 +0300

    Создал файл Readme.md

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -1,17 +1,17 @@
 ﻿Console.Write("Введите количество элементов из которых состоит массив: ");
-int n = Convert.ToInt32(Console.ReadLine()); 
-string[] arr = new string[n]; 
+int a = Convert.ToInt32(Console.ReadLine()); 
+string[] arr = new string[a]; 
 void InputArray(string[] array)
 {
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < a; i++)
     {
         Console.WriteLine($"Введите элемент массива №{i + 1}: ");
         array[i] = Console.ReadLine();
     }
     Console.WriteLine("Вы ввели следующий массив:");
     Console.Write($"[{array[0]}");
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < a; i++)
     {
         Console.Write($",{array[i]}");
     }
